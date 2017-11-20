f4e02cf3cb8abae32f5ed177eb67c2b3b150df62
Zhihan Chen <CHEN.Zhihan@outlook.com>
2017-11-20 21:08:21 +0800

    complete edit message

--- public/javascripts/myscripts.js
+++ public/javascripts/myscripts.js
@@ -1,6 +1,38 @@
 
-var app = angular.module("ChatterBox", []);
-app.controller("logIn", function($scope, $http) {
+var app = angular.module("ChatterBox", []); 
+
+
+app.factory("userPage", () => {
+    return {
+        "user": "",
+        "page": "",
+        "partialPage": ""
+    };
+})
+
+function loadMain($scope, res) {
+    $scope.userPage.user = res.data.user;
+    $scope.userPage.page = "main";
+    $scope.userPage.partialPage = "empty";
+}
+
+app.controller("viewControl", ($scope, $http, userPage) => {
+    $scope.userPage = userPage;
+    console.log("send GET to /load");
+    $http.get("/load", {})
+    .then((res) => {
+        if (res.data.error) {
+            console.log("load error");
+            $scope.userPage.page = "login";
+        } else {
+            console.log("load success");
+            console.log(res.data);
+            loadMain($scope, res);
+        }
+    });
+});
+
+app.controller("logIn", function($scope, $http, userPage) {
     $scope.logIn = () => {
         if (typeof $scope.username === "undefined") {
             alert("Please enter a valid username");
@@ -10,11 +42,65 @@ app.controller("logIn", function($scope, $http) {
             alert("Please enter a valid password");
             return;
         }
+        console.log("send POST to /login");
         $http.post("/login", {
             "name": $scope.username,
             "password": $scope.password
         }).then((res) => {
+            console.log("login returns");
             console.log(res.data);
+            loadMain($scope, res);
         })
     }
 });
+
+app.controller("main", ($scope, $http, userPage) => {
+    $scope.userPage = userPage;
+    $scope.logOut = () => {
+        $http.get("/logout", {})
+        .then((res) => {
+            console.log("logout returns");
+            $scope.userPage.page = "login";
+        }).catch((err) => {
+            console.log("logout failed");
+            console.log(err);
+        })
+    };
+    $scope.getUserInfo = () => {
+        $scope.userPage.partialPage = "info";
+    }
+    $scope.userPage.partialPage = "empty";
+});
+
+app.controller("info", ($scope, $http, userPage) => {
+    $scope.userPage = userPage;
+    $http.get("/getuserinfo", {})
+    .then((res) => {
+        console.log("getuserinfo returns");
+        if (res.error) {
+            console.log("getuserinfo remote error");
+            return;
+        }
+        console.log(res.data);
+        $scope.data = res.data;
+        delete $scope.data["error"];
+    }).catch((err) => {
+        console.log("getuserinfo local error");
+        console.log(err);
+    });
+
+    $scope.save = () => {
+        console.log($scope.data);
+        $http.put("/saveuserinfo", $scope.data).then((res) => {
+            if (res.error) {
+                alert("Error update userinfo");
+                return;
+            }
+            console.log("update userinfo successfully");
+            $scope.userPage.partialPage = "empty";
+        }).catch((err) => {
+            console.log("saveuserinfo local error");
+            console.log(err);
+        })
+    }
+})
--- routes/chats.js
+++ routes/chats.js
@@ -1,12 +1,11 @@
 
 var express = require('express');
 var mongo = require("mongodb");
-
+var monk = require("monk");
 var router = express.Router();
 
 
 function getAllFriends(collection, nameToLastMsgId) {
-    console.log(nameToLastMsgId);
     return collection.find({
             name: {
                 "$in": Object.keys(nameToLastMsgId)
@@ -16,47 +15,170 @@ function getAllFriends(collection, nameToLastMsgId) {
         return friendsNameId.reduce((result, x) => (
             result[x._id] = {
                 "name": x.name,
-                "lastMsgId": nameToLastMsgId[x.name]
+                // "lastMsgId": nameToLastMsgId[x.name]
+                "msgNotReceived": 0
             }
         , result), {});
+    });
+}
+
+function getUser(promise) {
+    return promise.then((doc) => {
+        if (doc === null) {
+            throw "No such user";
+        }
+        var user = {};
+        user.id = doc._id;
+        user.icon = doc.icon;
+        user.name = doc.name;
+        console.log(doc);
+        user.friends = doc.friends.reduce((result, friend) => (result[friend.name] = friend.lastMsgId, result), {});
+        return user;
+    });
+}
+
+function getOnloadData(promise, collection) {
+    var user = getUser(promise);
+    var friends = user.then((u) => getAllFriends(collection, u.friends));
+    return Promise.all([user, friends]).then((userFriends) => {
+        u = userFriends[0];
+        u.friends = userFriends[1];
+        return {
+            "user": u,
+            "error": false
+        };
     });
 }
 
 router.post("/login", (req, res) => {
     var collection = req.db.get("userList");
     console.log(req.body);
-    var getUser = collection.findOneAndUpdate(req.body, 
-                                              {"$set": {"status": "online"}}, 
-                                             {"fields": {
-                                                 "name": 1, "icon": 1, "friends": 1
-                                             }})
+    var promise = collection.findOneAndUpdate(req.body, {"$set": {"status": "online"}}, 
+                                                {"fields": {
+                                                    "name": 1, 
+                                                    "icon": 1, 
+                                                    "friends": 1}});
+    getOnloadData(promise, collection)
     .then((doc) => {
-        if (doc === null) {
-            throw err;
-        }
-        req.session.userId = doc._id;
-        var user = {};
-        user.id = doc._id;
-        user.icon = doc.icon;
-        user.name = doc.name;
-        user.friends = doc.friends.reduce((result, friend) => (result[friend.name] = friend.lastMsgId, result), {});
-        return user;
-    });
-    var getFriends = getUser.then((user) => getAllFriends(collection, user.friends));
-    Promise.all([getUser, getFriends]).then((userFriends) => {
-        user = userFriends[0];
-        user.friends = userFriends[1];
-        res.send({
-            "user": user,
-            "error": false
-        });
+        req.session.userId = doc.user.id;
+        res.send(doc);
     }).catch((err) => {
         console.log(err);
         res.send({
             "user": {},
             "error": true
         });
-    });
+    })
 });
+
+router.get("/load", (req, res) => {
+    if (req.session.userId) {
+        console.log(req.session.userId + " in session");
+        var collection = req.db.get("userList");
+        var query = {
+            "_id": new mongo.ObjectID(req.session.userId)
+        };
+        var fields = {
+            "fields": {
+                "name": 1,
+                "icon": 1,
+                "friends": 1,
+                "_id": 0
+            }
+        };
+        var promise = collection.findOne(query, fields);
+        getOnloadData(promise, collection)
+        .then((doc) => {
+            res.send(doc);
+        }).catch((err) => {
+            console.log(err);
+            res.send({
+                "user": {},
+                "error": true
+            });
+        })
+    } else {
+        console.log("userID not in session");
+        res.send({
+            "user": {},
+            "error": true
+        });
+    }
+});
+
+router.get("/logout", (req, res) => {
+    if (req.session.userId) {
+        var query = {
+            "_id": new mongo.ObjectID(req.session.userId)
+        };
+        var collection = req.db.get("userList");
+        console.log(query);
+        collection.update(query, {"$set": {"status": "offline"}})
+        .then((doc) => {
+            console.log("User logout successfully");
+            console.log(doc);
+            req.session.userId = null;
+            res.send({"error": false});
+        }).catch((err) => {
+            console.log(err);
+            res.send({"error": true});
+        })
+    } else {
+        console.log("Not logged in @ logout");
+        res.send({"error": true});
+    }
+
+});
+
+router.get("/getuserinfo", (req, res) => {
+    if (req.session.userId) {
+        var query = {
+            "_id": new mongo.ObjectID(req.session.userId)
+        };
+        var collection = req.db.get("userList");
+        var fields = {
+            "fields": {
+                "mobileNumber": 1,
+                "homeNumber": 1,
+                "address": 1,
+                "_id": 0
+            }
+        };
+        collection.findOne({query}, fields)
+        .then((doc) => {
+            console.log("getuserinfo successfully");
+            console.log(doc);
+            doc.error = false;
+            res.send(doc);
+        }).catch((err) => {
+            console.log(err);
+            res.send({"error": true});
+        });
+    } else {
+        console.log("Not logged in @ logout");
+        res.send({"error": true});
+    }
+})
+
+router.put("/saveuserinfo", (req, res) => {
+    console.log("receive saveinfo request");
+    console.log(req.body);
+    if (req.session.userId) {
+        var query = {
+            "_id": new mongo.ObjectID(req.session.userId)
+        };
+        console.log(query)
+        var collection = req.db.get("userList");
+        collection.update(query, {"$set": req.body})
+        .then((doc) => {
+            console.log(doc);
+            console.log("saveuserinfo successfully");
+            res.send({"error": false});
+        }).catch((err) => {
+            console.log(err);
+            res.send({"error": true});
+        })
+    }
+})
 
 module.exports = router;
